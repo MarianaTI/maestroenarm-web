49808d87dde76b1bc468119f84b8b495d29c692b
Ángel Eduardo Ricardez Sánchez <eduardoricardezmexico@gmail.com>
2022-10-13 01:36:13 -0500

    Questions and Answers

---
+++ src/constants.js
@@ -0,0 +1,76 @@
+export default {
+  clinicalCases: [
+    {
+      label:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pulvinar sem in rhoncus euismod. In quis porta ex, vitae dapibus lacus. Duis cursus orci in mauris pretium volutpat. Curabitur congue lacus et fringilla dignissim. Pellentesque ullamcorper mollis purus vel fermentum. Aenean et eros finibus, convallis est eget, convallis arcu. Sed ullamcorper nulla justo, et molestie mi feugiat vitae. Aliquam porta ipsum eu interdum lobortis. Sed et elit sem. Aliquam luctus ullamcorper libero, et sollicitudin metus tempor malesuada. Proin porta porta volutpat. Curabitur a nibh vitae libero egestas porttitor.",
+      questions: [
+        {
+          label: "Pregunta uno",
+          answers: [
+            {
+              label: "Respuesta uno",
+              isCorrect: false,
+            },
+            {
+              label: "Respuesta dos",
+              isCorrect: true,
+            },
+            {
+              label: "Respuesta tres",
+              isCorrect: false,
+            },
+            {
+              label: "Respuesta cuatro",
+              isCorrect: false,
+            },
+          ],
+        },
+        {
+          label: "Pregunta dos",
+          answers: [
+            {
+              label: "Respuesta uno",
+              isCorrect: false,
+            },
+            {
+              label: "Respuesta dos",
+              isCorrect: true,
+            },
+            {
+              label: "Respuesta tres",
+              isCorrect: false,
+            },
+            {
+              label: "Respuesta cuatro",
+              isCorrect: false,
+            },
+          ],
+        },
+        {
+          label: "Pregunta tres",
+          answers: [
+            {
+              label: "Respuesta uno",
+              isCorrect: false,
+            },
+            {
+              label: "Respuesta dos",
+              isCorrect: true,
+            },
+            {
+              label: "Respuesta tres",
+              isCorrect: false,
+            },
+            {
+              label: "Respuesta cuatro",
+              isCorrect: false,
+            },
+          ],
+        },
+      ],
+      feedback: {
+        text: "Esto es un feedback, te estoy explicando que la respuesta correcta era blablabla porque es logico que el blablabla es lo mejor",
+      },
+    },
+  ],
+};
