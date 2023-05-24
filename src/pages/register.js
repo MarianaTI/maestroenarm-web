c702344a6ca0d0e96c408a9441d3eca601d4057c
CynthiaVerde05 <cynthiavc78@gmail.com>
2023-05-24 17:02:35 -0500

    Cambios de diseño

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,6 +1,6 @@
 import {Grid} from "@mui/material";
-import {RegisterGrid, FormStyled} from "../styles/Register.style";
-
+import {RegisterGrid, FormStyled, ImageRegister, ButtonStyled, CardStyle, Question, LinkText} from "../styles/Register.style";
+import CustomInput from "../components/CustomInput";
 
 const Register = () => {
 
@@ -8,12 +8,31 @@ const Register = () => {
         <>
         <RegisterGrid container component="main">
             <Grid item xs={12} md={6} sm={6}>
-                <h1>Registro</h1>
-                <span>¡Bienvenido! Ingrese sus datos</span>
-                <FormStyled>
-
-                </FormStyled>
-            </Grid>   
+                <ImageRegister/>
+                <CardStyle>
+                    <h1>Registro</h1>
+                    <span>¡Bienvenido! Ingrese sus datos</span>
+                    <FormStyled>
+                        <span>Nombre</span>
+                        <CustomInput/>
+                        <span>Apellido</span>
+                        <CustomInput/>
+                        <span>Correo electronico</span>
+                        <CustomInput/>
+                        <span>Contraseña</span>
+                        <CustomInput/>
+                        <span>Confirmar contraseña</span>
+                        <CustomInput/>
+                    </FormStyled>
+                    <ButtonStyled>Iniciar</ButtonStyled>
+                    <Question>
+                        <span>
+                            ¿Ya tienes cuenta?
+                            <LinkText href="#">Iniciar sesión</LinkText>
+                        </span>
+                    </Question>
+                </CardStyle>
+            </Grid> 
         </RegisterGrid>
             
         </>
--- src/styles/Register.style.js
+++ src/styles/Register.style.js
@@ -10,12 +10,12 @@ import {
 } from "@mui/material";
  
 export const RegisterGrid = styled(Grid)`
-    height: 100vh;
+    height: 110vh;
     background-color: #f4f4f4;
     width: 100%;
-    padding: 8px;
+    padding: 0px;
     & h1 {
-    font-size: 32px;
+    font-size: 35px;
     font-weight: 600;
     color: #052970;
   }
@@ -24,9 +24,59 @@ export const FormStyled = styled.div`
   display: flex;
   flex-direction: column;
   width: 65%;
-  height: 100vh;
+  height: 60vh;
   font-size: 14px;
   & span {
     color: #000000;
   }
+`;
+export const ButtonStyled = styled(Button)`
+  width: 340px;
+  height: 40px;
+  background-color: #052970;
+  border-radius: 15px;
+  font-size: 12px;
+  color: #ffffff;
+  font-family: "Poppins";
+  font-weight: 500;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.3);
+  padding: 15px;
+  margin: 20px;
+  &:hover {
+    background: #05297099;
+  }
+`;
+export const CardStyle = styled(Box)`
+  max-width: 500px;
+  height: 500px;
+  margin: 10%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  & span {
+    color: #00000090;
+    font-weight: 500;
+    padding: 5px;
+    letter-spacing: -0.01em;
+    font-feature-settings: "calt" off;
+  }
+`;
+export const Question = styled.div`
+  padding: 5px;
+  & span {
+    display: inline;
+  }
+`;
+export const LinkText = styled(Link)`
+  padding-left: 16px;
+  font-size: 14px;
+  color: #052970;
+  font-weight: 600;
+  text-decoration: none;
+  display: inline;
+`;
+
+export const ImageRegister = styled(Grid)`
+  background-image: url(./registerphoto2.svg);
+  
 `;
