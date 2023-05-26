d51dc453cd5e0615996c6ccbbb6e1d6bdd4ca9c0
unknown <syra478xd@gmail.com>
2023-05-25 20:37:34 -0600

    Responsivo

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -1,17 +1,21 @@
 import React from "react";
 import styles from "../styles/demo.module.css";
+import Responsivo from '../styles/Result.style.js';
+import stylesContainer from "../styles/Home.module.css";
 import { Button } from "@mui/material";
 
 const Bienvenido = () => {
   return (
-    <div  id={styles.contenedorCentrado}>
+    
+      <div  id={styles.contenedorCentrado}>
       <h1 className={styles.bienvenido}>Bienvenido a Maestro ENARM</h1>
       <div className={styles.imagenDoctor} />
-      <button className={styles.boton}>
+      <a className={styles.boton} href="/">
       <span>Jugar Demo</span>
-      </button>
+      </a>
       <p className={styles.saltarDemo}>Desea saltar la DEMO?  <a href="#">Saltar</a> </p>
     </div>
+    
   );
 };
 
--- src/styles/Result.style.js
+++ src/styles/Result.style.js
@@ -12,55 +12,115 @@ export const TextContainerResult = styled.div`
     letter-spacing: -0.01em;
     font-feature-settings: 'calt' off;
   }
+  @media (max-width: 768px) {
+    margin: 1%;
+    font-size: 18px;
+
+    & span:nth-of-type(1),
+    & span:nth-of-type(2),
+    & span:nth-of-type(3) {
+      font-size: 14px;
+      margin: 4px;
+    }
+  }
+
+  @media (max-width: 995px) {
+    margin: 1%;
+    font-size: 18px;
+
+    & span:nth-of-type(1),
+    & span:nth-of-type(2),
+    & span:nth-of-type(3) {
+      font-size: 16px;
+      margin: 6px;
+    }
+  }
 `;
 
 export const Container = styled.div`
-    width: 620px;
-    margin-left: 10%;
+     margin-left: 10%;
+    @media (max-width: 768px) {
+        margin-left: 5%;
+    }
 `;
 
 export const ContainerResult = styled.div`
     box-sizing: border-box;
     position: absolute;
-    width: 1020px;
+    width: 90%;
+    max-width: 1020px;
     height: 90px;
     background: #FCFCFC;
     border: 0.5px solid #585555;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 10px;
     margin: auto;
+  
+    
     & span {
-    font-family: 'Poppins';
-    font-style: normal;
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 150%;
-    text-align: center;
-    letter-spacing: -0.01em;
-    font-feature-settings: 'calt' off;
-    margin: 1.4%;
-    margin-left: 3%;
-  }
-  & span:nth-of-type(1) {
-    display: flex;
-    color: #000000;
-  }
-  & span:nth-of-type(2) {
-    font-size: 16px;
-    display: inline;
-    color: rgba(0, 0, 0, 0.5);
-  }
-  & span:nth-of-type(3) {
-    font-size: 16px;
-    color: rgba(0, 0, 0, 0.5);
-    margin-left: 78%;
-  }
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 20px;
+        line-height: 150%;
+        text-align: center;
+        letter-spacing: -0.01em;
+        font-feature-settings: 'calt' off;
+        margin: 1.2%;
+        margin-left: 3%;
+    }
+    & span:nth-of-type(1) {
+        display: flex;
+        color: #000000;
+    }
+    & span:nth-of-type(2) {
+        font-size: 16px;
+        display: inline;
+        color: rgba(0, 0, 0, 0.5);
+    }
+    & span:nth-of-type(3) {
+        font-size: 16px;
+        color: rgba(0, 0, 0, 0.5);
+        margin-left: 78%;
+    }
+
+    @media (max-width: 995px) {
+    width: 90%;
+    max-width: 700px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 8px;
+
+    & span:nth-of-type(1),
+    & span:nth-of-type(2),
+    & span:nth-of-type(3) {
+      font-size: 14px;
+      margin: 4px;
+    }
+   }
+
+    @media (max-width: 768px) {
+        width: 90%;
+        max-width: 500px;
+        margin-left: auto;
+        margin-right: auto;
+        padding: 8px;
+
+        & span:nth-of-type(1),
+        & span:nth-of-type(2),
+        & span:nth-of-type(3) {
+            font-size: 14px;
+            margin: 4px;
+        }
+    }
 `;
+
 
 export const TextStatic = styled.div`
     margin-top: 112px;
     display: flex;
-  width: 900px;
+    width: 90%;
+    max-width: 900px;
   margin-left: 3%;
 
   
@@ -85,24 +145,47 @@ export const TextStatic = styled.div`
     color: rgba(0, 0, 0, 0.5);
     margin-left: 78%;
   }
+  @media (max-width: 768px) {
+    flex-wrap: wrap;
+    margin-left: 1%;
+
+    & span:nth-of-type(1),
+    & span:nth-of-type(2) {
+      font-size: 14px;
+      margin: 2px;
+    }
+
+    & span:nth-of-type(3) {
+      font-size: 14px;
+      margin-left: auto;
+    }
+  }
+
+  @media (max-width: 995px) {
+    margin-left: 2%;
+
+    & span:nth-of-type(1) {
+      font-size: 14px;
+    }
+
+    & span:nth-of-type(2) {
+      font-size: 14px;
+      margin-left: 68%;
+    }
+  }
 `;
 
 export const Buttom = styled.div`
-margin:16px;
-  display: flex;
-  flex-direction: row;
+display: flex;
   justify-content: center;
   align-items: center;
   padding: 16px 24px;
-  gap: 8px;
-  margin-left: 50%;
-  
-    width: 397px;
-    height: 46px;
-    left: 448px;
-    top: 852px;
-    background: #052970;
-    border-radius: 15px;
+  margin: 16px;
+  width: 90%;
+  max-width: 397px;
+  height: 46px;
+  background: #052970;
+  border-radius: 15px;
 
     & span {
     font-family: 'Inter';
@@ -116,4 +199,7 @@ margin:16px;
     font-feature-settings: 'calt' off;
     color: #FFFFFF;
   } 
+  @media (max-width: 768px) {
+    width: 80%;
+  }
 `;
