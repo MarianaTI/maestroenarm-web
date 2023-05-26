35c3f87e1d9b04a4cd77373e7c47cb1756caf646
syra478xd@gmail.com <zeen.cr@gmail.com>
2023-05-25 23:27:31 -0600

    modify navbar and add main layout

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,50 @@
+import Link from "next/link"
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import HomeIcon from '@mui/icons-material/Home';
+import PersonIcon from '@mui/icons-material/Person';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import MenuIcon from '@mui/icons-material/Menu';
+import { CustomIconButton, CustomNavLi, CustomNavUl, EnarmIcon, Header } from "./index.style";
+
+const links = [
+    {
+        label: 'Examenes',
+        route: '/',
+        icon: <HomeIcon />
+    },
+    {
+        label: 'Estadisticas',
+        route: '/estadisticas',
+        icon: <TrendingUpIcon />
+    },
+    {
+        label: 'Ranking',
+        route: '/ranking',
+        icon: <BarChartIcon />
+    },
+    {
+        label: "Nosotros",
+        route: "/nosotros",
+        icon: <PersonIcon />
+    }
+];
+
+export default function Navbar() {
+    return (
+        <Header>
+            <nav>
+                <CustomNavUl>
+                    <EnarmIcon> Maestro ENARM</EnarmIcon>
+                    {links.map(({ label, route, icon }) => (
+                        <CustomNavLi  key={route}>
+                            {icon}
+                            <Link href={route}>{label}</Link>
+                        </CustomNavLi>))}
+                    <CustomIconButton>
+                        <MenuIcon />
+                    </CustomIconButton>
+                </CustomNavUl>
+            </nav>
+        </Header>
+    );
+}
--- src/pages/profile-settings.js
+++ src/pages/profile-settings.js
@@ -1,6 +1,7 @@
 import { Avatar } from '@mui/material';
 import { AvatarContainer, Button, ButtonGroup, ProfileSettingsContainer } from '../styles/ProfileSettings';
 import { ProfileField } from '../components/ProfileField';
+import MainLayout from '../layouts/MainLayout';
 
 const profileFields = [
     { label: 'Nombre de Usuario', text: 'ZeeN' },
@@ -16,19 +17,21 @@ const buttons = [
 ];
 
 export default function ProfileSettings() {
-    return <>
-        <AvatarContainer>
-            <Avatar sx={{ width: 132, height: 132 }} alt="Adrian Mis" src="" />
-            <p>Cuenta Básica</p>
-        </AvatarContainer>
-        <ProfileSettingsContainer>
-            {profileFields.map(({ label, text }) => <ProfileField key={label} label={label} text={text}></ProfileField>)}
-        </ProfileSettingsContainer>
-        <ButtonGroup>
-            {buttons.map(button => <Button
-                key={button.text}
-                bg={button.bgColor}
-                color="#F4F4F4">{button.text}</Button>)}
-        </ButtonGroup>
-    </>
+    return (
+        <MainLayout>
+            <AvatarContainer>
+                <Avatar sx={{ width: 132, height: 132 }} alt="Adrian Mis" src="" />
+                <p>Cuenta Básica</p>
+            </AvatarContainer>
+            <ProfileSettingsContainer>
+                {profileFields.map(({ label, text }) => <ProfileField key={label} label={label} text={text}></ProfileField>)}
+            </ProfileSettingsContainer>
+            <ButtonGroup>
+                {buttons.map(button => <Button
+                    key={button.text}
+                    bg={button.bgColor}
+                    color="#F4F4F4">{button.text}</Button>)}
+            </ButtonGroup>
+        </MainLayout>
+    );
 }
