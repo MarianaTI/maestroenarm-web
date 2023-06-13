da0034762aabd36c8670f9f90c60a93ee0d16fda
Ángel Eduardo Ricardez Sánchez <eduardoricardezmexico@gmail.com>
2023-06-13 13:45:08 -0600

    Menu

--- src/components/GameField/index.js
+++ src/components/GameField/index.js
@@ -1,10 +1,10 @@
 import { FieldDecoration, Label, FieldWrapper } from "./index.style";
 
-export const GameField = ({ label, onClick }) => {
-    return (
-        <FieldWrapper onClick={onClick} >
-            <FieldDecoration />
-            <Label>{label}</Label>
-        </FieldWrapper>
-    );
-}
+export const GameField = ({ label, onClick, disabled }) => {
+  return (
+    <FieldWrapper onClick={onClick} disabled={disabled}>
+      <FieldDecoration />
+      <Label>{label}</Label>
+    </FieldWrapper>
+  );
+};
--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -5,23 +5,29 @@ import Layout from "../layout/index";
 import { TitleGameContainer } from "../../styles/Game";
 
 export default function Game() {
-    const [open, setOpen] = useState(false);
-    return <>
-        <Layout>
-            <TitleGameContainer>
-                <h1 style={{ fontWeight: '500' }}>MODO PRÁCTICA</h1>
-                <p>Feedback déspues de cada pregunta</p>
-            </TitleGameContainer>
-            <GameField label='Aleatorio' />
-            <GameField label='Por Categoría' />
-            <TitleGameContainer>
-                <h1 style={{ fontWeight: '500' }}>MODO MULTIJUGADOR</h1>
-                <p>Feedback al finalizar el exámen</p>
-                <p>AMISTOSO</p>
-            </TitleGameContainer>
-            <GameField label='Aleatorio' />
-            <GameField label='Por Categoría' onClick={() => setOpen(true)} />
-            <GameSettingsModal isOpen={open} />
-        </Layout>
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Layout>
+        <TitleGameContainer>
+          <h1 style={{ fontWeight: "500" }}>MODO PRÁCTICA</h1>
+          <p>Feedback déspues de cada pregunta</p>
+        </TitleGameContainer>
+        <GameField label="Aleatorio" />
+        <GameField label="Por Categoría" disabled />
+        <TitleGameContainer>
+          <h1 style={{ fontWeight: "500" }}>MODO MULTIJUGADOR</h1>
+          <p>Feedback al finalizar el exámen</p>
+          <p>AMISTOSO</p>
+        </TitleGameContainer>
+        <GameField label="Aleatorio" />
+        <GameField
+          label="Por Categoría"
+          disabled
+          onClick={() => setOpen(true)}
+        />
+        <GameSettingsModal isOpen={open} />
+      </Layout>
     </>
+  );
 }
