99f4d151c71005bf9695ddd5559802c6a7dbcacb
Ángel Eduardo Ricardez Sánchez <eduardoricardezmexico@gmail.com>
2023-06-20 13:21:37 -0600

    navbar-refactor

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,16 +1,22 @@
-import { useRouter } from 'next/router';
-import Navbar from '../components/Nab';
-import { Container } from './index.style'
-import NavDrawer from '../components/NavDrawer';
+import { useRouter } from "next/router";
+import Navbar from "../components/Nab";
+import { Container } from "./index.style";
+import NavDrawer from "../components/NavDrawer";
 
 export default function Layout({ children }) {
-    const route = useRouter()
-    return <>
-        {route.pathname === '/' || !route.pathname.match(/sesion|register|demo|test/g) && <>
-            <Navbar /><NavDrawer />
-        </>}
-        <Container>
-            {children}
-        </Container>
+  const route = useRouter();
+  const hasNavbar =
+    route.pathname === "/" ||
+    !route.pathname.match(/sesion|register|demo|test/g);
+  return (
+    <>
+      {hasNavbar && (
+        <>
+          <Navbar />
+          <NavDrawer />
+        </>
+      )}
+      <Container>{children}</Container>
     </>
+  );
 }
--- src/layouts/index.style.js
+++ src/layouts/index.style.js
@@ -4,7 +4,9 @@ export const Container = styled.div`
   max-width: 1440px;
   margin: auto;
   padding: 0 32px;
-  
+
+  /* ${(props) => props.hasNavbar && "padding-top:100px;"} */
+
   @media screen and (max-width: 576px) {
     padding: 0 16px;
   }
