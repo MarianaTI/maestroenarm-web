ae297754a81933a48ac98b9eaac0785a07005ea2
Sugey127 <sugey.rosado@buencodigo.dev>
2023-06-29 16:49:22 -0600

    feat(): Style Collapse Component

---
+++ src/components/Collapse/index.style.js
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  padding: 10px;
+  overflow: hidden;
+  height: auto;
+
+  @media (max-width: 768px) {
+    margin-left: 5%;
+  }
+`;
+
+export const Typography = styled.div`
+     font-family: "Poppins";
+`;
