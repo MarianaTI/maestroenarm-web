b5532d8a53b3d5eadd35775ba7ed1470c9c4c2d1
Ángel Eduardo Ricardez Sánchez <eduardoricardezmexico@gmail.com>
2023-06-30 18:18:52 -0600

    feat(): multiplayer el origen jsjsjs

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -2,19 +2,14 @@ import { createContext, useContext } from "react";
 import { useFirebaseAuth } from "../hooks/firebaseAuth";
 
 const authContext = createContext({
-    user: null,
-    loading: true,
-    closeSesion: () => {}
-})
+  user: null,
+  loading: true,
+  closeSesion: () => {},
+});
 
-export const useAuth = () => useContext(authContext)
+export const useAuth = () => useContext(authContext);
 
 export const AuthContextProvider = ({ children }) => {
-    const auth = useFirebaseAuth()
-    return (
-        <authContext.Provider value={auth}>
-            {children}
-        </authContext.Provider>
-    )
-}
-
+  const auth = useFirebaseAuth();
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+};
