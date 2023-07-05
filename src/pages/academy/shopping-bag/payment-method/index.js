6c4d635117fa8accc6cc5d39ca092e8955647e43
Mariana Trujillo Islas <marianatrujillo27@hotmail.com>
2023-07-05 12:53:02 -0600

    refactor(): stripe operation

--- src/pages/academy/shopping-bag/payment-method/index.js
+++ src/pages/academy/shopping-bag/payment-method/index.js
@@ -1,5 +1,5 @@
-import { Elements } from '@stripe/react-stripe-js';
-import React, { useEffect } from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import React from "react";
 import {
   CardContainer,
   CompletePayment,
@@ -9,7 +9,6 @@ import {
   MainContainer,
   PayContainer,
   PaymentContainer,
-  RowInputs,
   TitleContainer,
 } from "../../../../styles/PaymentMethod.style";
 import CustomInput from "../../../../components/CustomInput";
@@ -21,175 +20,134 @@ import Link from "next/link";
 import CustomShoppingDetails from "../../../../components/CustomShoppingDetails";
 import CustomCalculateTotal from "../../../../components/CustomCalculateTotal";
 import { useSelector } from "react-redux";
-import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromise = loadStripe('pk_test_51NQEZFEgjOGrqMGrKaOwcNLpCuvostnvfCEvigbYUI8tFogD1Jv2PVoQfFaiD77tOhF1Zyh4vYoasX7bABG6QtOK00qDnV4jat');
-
+const stripePromise = loadStripe(
+  "pk_test_51NQEZFEgjOGrqMGrKaOwcNLpCuvostnvfCEvigbYUI8tFogD1Jv2PVoQfFaiD77tOhF1Zyh4vYoasX7bABG6QtOK00qDnV4jat"
+);
 
 const paymentSchema = yup.object().shape({
   cardName: yup.string().required("Este dato es requerido"),
-  cardNumber: yup
-    .string()
-    .required("Este dato es requerido")
-    .matches(
-      /^\d{4}\s\d{4}\s\d{4}\s\d{4}$/,
-      "Ingresa el número con el formato correcto"
-    )
-    .max(19),
-  expiration: yup
-    .string()
-    .required("Este dato es requerido")
-    .matches(
-      /^(0[1-9]|1[0-2])\/([0-9]{2})$/,
-      "Ingresa el número con el formato correcto"
-    )
-    .max(5),
-  cvc: yup
-    .string()
-    .required("Este dato es requerido")
-    .matches(/^\d{3}$/, "Ingresa el número con el formato correcto")
-    .min(3)
-    .max(4),
 });
 
-const formatCardNumber = (value) => {
-  return value
-    .replace(/[^\d]/g, "")
-    .substring(0, 16)
-    .replace(/(.{4})/g, "$1 ")
-    .trim();
-};
 
-const formatExpirationDate = (value) => {
-  return value
-    .replace(/[^\d]/g, "")
-    .substring(0, 4)
-    .replace(/(.{2})/, "$1/");
-};
-export default function PaymentMethod() {
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
   const {
     control,
     handleSubmit,
-    watch,
-    setValue,
     formState: { errors },
   } = useForm({
     defaultValues: {
       cardName: "",
-      cardNumber: "",
-      expiration: "",
-      cvc: "",
     },
     resolver: yupResolver(paymentSchema),
   });
-  const cardNumber = watch("cardNumber");
-  const expiration = watch("expiration");
-  const cvc = watch("cvc");
 
-  useEffect(() => {
-    const formattedCardNumber = formatCardNumber(cardNumber);
-    setValue("cardNumber", formattedCardNumber);
-  }, [cardNumber, setValue]);
+  const onSubmit = async (event) => {
+    event.preventDefault();
 
-  useEffect(() => {
-    const formattedExpirationDate = formatExpirationDate(expiration);
-    setValue("expiration", formattedExpirationDate);
-  }, [expiration, setValue]);
-
-  useEffect(() => {
-    const limitedCvc = cvc.substring(0, 3);
-    setValue("cvc", limitedCvc);
-  }, [cvc, setValue]);
-
-  const onSubmit = async (values) => {
     if (!stripe || !elements) {
-        return; // retorna si Stripe no ha cargado aún.
+      return; // Retorna si Stripe no ha cargado aún.
     }
+
+    console.log("Form Values:", values); // Aquí imprimimos los datos del formulario.
 
     const cardElement = elements.getElement(CardElement);
 
-    const {error, paymentMethod} = await stripe.createPaymentMethod({
-        type: 'card',
-        card: cardElement,
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: cardElement,
     });
 
     if (error) {
-        console.log('[error]', error);
+      console.log("Error al crear el método de pago:", error);
     } else {
-        console.log('[PaymentMethod]', paymentMethod);
-        // Aquí puedes enviar `paymentMethod.id` a tu servidor para procesar el pago.
+      console.log("Método de pago creado:", paymentMethod);
+      
+        
     }
-};
-
+  };
 
   const product = useSelector((state) => state.product.currentProduct);
+  
 
   return (
-    <Elements stripe={stripePromise}>
-    <ContainerForm onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <MainContainer>
-          <h1>Continua con tu compra</h1>
-          <span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua
-          </span>
-        </MainContainer>
-        <PayContainer>
-          <span>Método de pago</span>
-          <CardContainer>
-            <TitleContainer>
-              <IconStyled />
-              <span>Tarjeta de crédito/débito</span>
-            </TitleContainer>
-            <FormStyled>
-              <CustomInput
-                label="Nombre del propietario"
-                name="cardName"
-                control={control}
-                placeholder="Nombre del propietario"
-                error={errors.cardName?.message}
-              />
-              <CardElement options={{
-                  style: {
-                    base: {
-                      fontSize: '14px',
-                      color: '#424770',
-                      '::placeholder': {
-                        color: '#aab7c4',
+      <ContainerForm onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <MainContainer>
+            <h1>Continua con tu compra</h1>
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua
+            </span>
+          </MainContainer>
+          <PayContainer>
+            <span>Método de pago</span>
+            <CardContainer>
+              <TitleContainer>
+                <IconStyled />
+                <span>Tarjeta de crédito/débito</span>
+              </TitleContainer>
+              <FormStyled>
+                <CustomInput
+                  label="Nombre del propietario"
+                  name="cardName"
+                  control={control}
+                  placeholder="Nombre del propietario"
+                  error={errors.cardName?.message}
+                />
+                <CardElement
+                  options={{
+                    style: {
+                      base: {
+                        fontSize: "14px",
+                        color: "#424770",
+                        "::placeholder": {
+                          color: "#aab7c4",
+                        },
+                      },
+                      invalid: {
+                        color: "#9e2146",
                       },
                     },
-                    invalid: {
-                      color: '#9e2146',
-                    },
-                  },
-                }} />
-            </FormStyled>
-          </CardContainer>
-        </PayContainer>
-        <CustomShoppingDetails
-          productName={product.name}
-          productTopics={product.topics}
-          productPrice={product.price}
-        />
-      </div>
-      <PaymentContainer>
-        <CompletePayment>
-          <CustomCalculateTotal
-            originalPrice={product.price}
-            totalPrice={product.price}
+                  }}
+                />
+              </FormStyled>
+            </CardContainer>
+          </PayContainer>
+          <CustomShoppingDetails
+            productName={product.name}
+            productTopics={product.topics}
+            productPrice={product.price}
           />
-          <span className="DetailText">
-            Al completar la compra, aceptas estas{" "}
-            <Link href="#" className="LinkText">
-              Condiciones de uso.
-            </Link>
-          </span>
-          <CustomButtonAcademy buttonText="Completar pago" type="submit" />
-        </CompletePayment>
-      </PaymentContainer>
-    </ContainerForm>
+        </div>
+        <PaymentContainer>
+          <CompletePayment>
+            <CustomCalculateTotal
+              originalPrice={product.price}
+              totalPrice={product.price}
+            />
+            <span className="DetailText">
+              Al completar la compra, aceptas estas{" "}
+              <Link href="#" className="LinkText">
+                Condiciones de uso.
+              </Link>
+            </span>
+            <CustomButtonAcademy buttonText="Completar pago" type="submit" />
+          </CompletePayment>
+        </PaymentContainer>
+      </ContainerForm>
+  );
+}
+
+export default function PaymentMethod() {
+  return (
+    <Elements stripe={stripePromise}>
+      <CheckoutForm />
     </Elements>
   );
 }
