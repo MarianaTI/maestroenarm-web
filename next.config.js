f6a552364e75f9abe879d5a38bc723149570be4f
syra478xd@gmail.com <zeen.cr@gmail.com>
2023-07-23 13:15:54 -0600

    fix() corrigiendo rendimiento de la pagina

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['m.media-amazon.com']
-  }
+  reactStrictMode: false,
+  swcMinify: true
 }
 
 module.exports = nextConfig
