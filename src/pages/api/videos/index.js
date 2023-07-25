7e97b9b9e139917e873b2ebec8c6829a953bb6d4
syra478xd@gmail.com <zeen.cr@gmail.com>
2023-07-25 14:00:23 -0600

    feat() filter is implemented on video pages academy

--- src/pages/api/videos/index.js
+++ src/pages/api/videos/index.js
@@ -1,10 +1,27 @@
 import cloudinary from "../../../server/cloudinary/config"
 
 export default async function handler(req, res) {
-    const { limit = 10, title } = req.query;
+    const { limit = 10, title = '', specialties = '' } = req.query;
     try {
+        if (specialties && title) {
+            const { resources } = await cloudinary.search.expression(
+                `resource_type:video AND (context.caption:${title} OR tags:${specialties})`)
+                .with_field('context')
+                .execute()
+            res.status(200).json(resources)
+        }
         if (title) {
-            const { resources } = await cloudinary.search.expression(`context.caption:${title}  AND resource_type:video`).with_field('context').execute()
+            const { resources } = await cloudinary.search.expression(
+                `resource_type:video AND context.caption:${title}`)
+                .with_field('context')
+                .execute()
+            res.status(200).json(resources)
+        }
+        if (specialties) {
+            const { resources } = await cloudinary.search.expression(
+                `resource_type:video AND tags:${specialties}`)
+                .with_field('context')
+                .execute()
             res.status(200).json(resources)
         }
         const { resources } = await cloudinary.search.expression(`resource_type:video`).with_field('context').max_results(limit).execute()
