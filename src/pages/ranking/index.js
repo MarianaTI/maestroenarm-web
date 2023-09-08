0d4f83516747175c731336d97a33dec39fab25a2
Sugey127 <sugey.rosado@buencodigo.dev>
2023-09-08 13:08:14 -0600

    feat(): rankin University

--- src/pages/ranking/index.js
+++ src/pages/ranking/index.js
@@ -3,31 +3,31 @@ import RankingCard from "../../components/RankingCard";
 import RankingList from "../../components/RankingList";
 import { RankingContainer } from "../../styles/Ranking.Style";
 import { db } from "../../services/firebase/config";
-import { collection, getDocs, query, where } from "@firebase/firestore";
+import { collection, getDocs, query } from "@firebase/firestore";
 
 export default function Ranking() {
 
-    const [filterItems, setFilterItems] = useState([])
-    let snapshot;
+  const [filterItems, setFilterItems] = useState([]);
 
-    async function getUserInformation() {
-		snapshot = await getDocs(query(collection(db, 'users')));
-		const userInformation = snapshot.docs.map(doc => doc.data());
-        userInformation.sort((a, b) => b.totalGameScore - a.totalGameScore);
-		setFilterItems(userInformation)
-	}
-    
-    useEffect(() => {
-		const debouncing = setTimeout(() => { getUserInformation() }, 500)
-		return () => clearTimeout(debouncing)
-	}, []);
+  async function getUserInformation() {
+    const snapshot = await getDocs(query(collection(db, 'users')));
+    const userInformation = snapshot.docs.map(doc => doc.data());
+    userInformation.sort((a, b) => b.totalGameScore - a.totalGameScore);
+    setFilterItems(userInformation)
+  }
 
-    const maxScoreUser = filterItems[0];
-    const secondMaxScoreUser = filterItems[1];
+  useEffect(() => {
+    const debouncing = setTimeout(() => { getUserInformation() }, 500)
+    return () => clearTimeout(debouncing)
+  }, []);
 
-    return <RankingContainer>
-        <div className="ranking-top-wrapper">
-        {secondMaxScoreUser && (
+  const maxScoreUser = filterItems[0];
+  const secondMaxScoreUser = filterItems[1];
+  const thirdMaxScoreUser = filterItems[2];
+
+  return <RankingContainer>
+    <div className="ranking-top-wrapper">
+      {secondMaxScoreUser && (
         <RankingCard
           rankedNumber={2}
           avatar
@@ -48,31 +48,31 @@ export default function Ranking() {
         />
       )}
 
-      {filterItems[2] && (
+      {thirdMaxScoreUser && (
         <RankingCard
           rankedNumber={3}
           avatar
-          name={filterItems[2].name}
-          university={filterItems[2].university}
-          specialty={filterItems[2].specialty}
-          score={filterItems[2].totalGameScore.toString()}
+          name={thirdMaxScoreUser.name}
+          university={thirdMaxScoreUser.university}
+          specialty={thirdMaxScoreUser.specialty}
+          score={thirdMaxScoreUser.totalGameScore.toString()}
         />
       )}
     </div>
-        <div className="ranking-wrapper">
-        {filterItems.slice(3).map((item, index) => (
-            (
-                <RankingList
-                  key={index}
-                  ranked_number={index + 4}
-                  avatar="none"
-                  name={item.name}
-                  score={item.totalGameScore.toString()}
-                  specialty={item.specialty}
-                  university={item.university}
-                />
-              ) 
-            ))}
-        </div>
-    </RankingContainer>
+    <div className="ranking-wrapper">
+      {filterItems.slice(3).map((item, index) => (
+        
+          <RankingList
+            key={index}
+            ranked_number={index + 4}
+            avatar="none"
+            name={item.name}
+            score={item.totalGameScore.toString()}
+            specialty={item.specialty}
+            university={item.university}
+          />
+        
+      ))}
+    </div>
+  </RankingContainer>
 }
--- src/pages/ranking/universities/index.js
+++ src/pages/ranking/universities/index.js
@@ -1,28 +1,71 @@
 import RankingCard from "../../../components/RankingCard";
 import RankingList from "../../../components/RankingList";
+import { db } from "../../../services/firebase/config";
 import { RankingContainer } from "../../../styles/Ranking.Style";
+import { collection, getDocs, query } from "@firebase/firestore";
+import { useEffect, useState } from "react";
 
 export default function Universities() {
+
+    const [filterItems, setFilterItems] = useState([]);
+
+    async function getUserInformation() {
+        const snapshot = await getDocs(query(collection(db, 'users')));
+        const userInformation = snapshot.docs.map(doc => doc.data());
+
+        const universityScore = [];
+        userInformation.forEach(user => {
+            if (universityScore[user.university]) {
+                universityScore[user.university] += user.totalGameScore;
+            } else {
+                universityScore[user.university] = user.totalGameScore;
+            }
+        })
+
+        const sortedUniversityScore = Object.entries(universityScore).map(([university, totalScore]) => ({ university, totalScore })).sort((a, b) => b.totalScore - a.totalScore);
+        setFilterItems(sortedUniversityScore);
+    }
+
+    useEffect(() => {
+        const debouncing = setTimeout(() => { getUserInformation(); }, 500);
+        return () => clearTimeout(debouncing);
+    }, []);
+
+    const maxScoreUser = filterItems[0];
+    const secondMaxScoreUser = filterItems[1];
+    const thirdMaxScoreUser = filterItems[2];
+
     return <RankingContainer>
         <div className="ranking-top-wrapper">
-            <RankingCard rankedNumber="2"
-                university="Universidad Tecnológica Metropolitana"
-                score="11,000"
+        {secondMaxScoreUser && (
+            <RankingCard rankedNumber={2}
+                university={secondMaxScoreUser.university}
+                score={secondMaxScoreUser.totalScore}
             />
-            <RankingCard rankedNumber="1"
-                university="Universidad Tecnológica Metropolitana"
-                score="12,000"
+        )}
+        {maxScoreUser && (
+            <RankingCard rankedNumber={1}
+                university={maxScoreUser.university}
+                score={maxScoreUser.totalScore}
             />
-            <RankingCard rankedNumber="3"
-                university="Universidad Tecnológica Metropolitana"
-                score="10,000"
+        )}
+        {thirdMaxScoreUser && (
+            <RankingCard rankedNumber={3}
+                university={thirdMaxScoreUser.university}
+                score={thirdMaxScoreUser.totalScore}
             />
+        )}
         </div>
         <div className="ranking-wrapper">
-            <RankingList ranked_number={4} score="9,000" university="Universidad Tecnológica Metropolitana" />
-            <RankingList ranked_number={5} score="9,000" university="Universidad Tecnológica Metropolitana" />
-            <RankingList ranked_number={6} score="9,000" university="Universidad Tecnológica Metropolitana" />
-            <RankingList ranked_number={7} score="9,000" university="Universidad Tecnológica Metropolitana" />
-        </div>
+        {filterItems.slice(3).map((item, index) => (
+            <RankingList 
+            key={index}
+            ranked_number={index +  4} 
+            score={item.totalScore.toString()}
+            university={item.university}
+            />
+            
+        ))}
+            </div>
     </RankingContainer>
 }
